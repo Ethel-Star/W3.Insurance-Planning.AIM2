c2def26f9d8a85bfed34bb8a77ce321282cca2ce
Ethel <etheltibeb@gmail.com>
2024-09-13 23:28:51 +0300

    Updating utils.py and notebook

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ class InsuranceDataUtils:
         self.cleaned_csv_file_path = cleaned_csv_file_path
     
     def load_data(self):
-        # Load data with specified dtypes
+        # Load data, specifying dtype for columns if needed or using low_memory=False
         dtype = {
             'Bank': str,
             'AccountType': str,
@@ -34,17 +34,16 @@ class InsuranceDataUtils:
         columns_to_check = ['Bank', 'AccountType', 'MaritalStatus', 'Gender']
         df_cleaned = df_cleaned.dropna(subset=columns_to_check)
         
-        # Fill missing values with mode or default values
+        # Fill missing values with the mean, median, or a specific value
         df_cleaned['Bank'] = df_cleaned['Bank'].fillna(df_cleaned['Bank'].mode()[0])
         df_cleaned['AccountType'] = df_cleaned['AccountType'].fillna(df_cleaned['AccountType'].mode()[0])
         df_cleaned['Gender'] = df_cleaned['Gender'].fillna(df_cleaned['Gender'].mode()[0])
         df_cleaned['VehicleType'] = df_cleaned['VehicleType'].fillna('Unknown')
         
-        # Forward fill for VehicleIntroDate
+        # Fill missing values with a specific date or forward/backward fill
         df_cleaned['VehicleIntroDate'] = df_cleaned['VehicleIntroDate'].ffill()
         
         return df_cleaned
     
     def save_data(self, df):
         df.to_csv(self.cleaned_csv_file_path, index=False)
-
